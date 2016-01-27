32b59073da41d4cac9a5d3124a596d7e31eac917
Flavio Ferreira <flaviometalvale@gmail.com>
2016-01-27 01:17:53 -0200

    Aula 2 - Set and Forget

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 new Vue({
     el: '#beerApp',
 
+    data: {
+        cervejarias: []
+    },
+
     ready: function() {
-        alert('beerApp');
+        var self = this;
+        //alert(this.$el.id);
+        self.$http.get('cervejarias.json', function(response)
+        {
+            self.cervejarias = response;
+            window.console.log(response);
+        });
+
     }
 });
